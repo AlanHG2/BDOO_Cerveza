9494a0c0d0bfb08d34145341309a0dcbc18def4c
AlanHG2 <alanhazelgg@gmail.com>
2024-08-30 15:51:00 -0600

    Modelos

---
+++ PrCerveza/src/model/Cerveza.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Cerveza {
+    private int id_Cerveza;
+    private String cer_Nombre;
+    private float cer_Graduacion;
+    private int id_Marca;
+
+    public Cerveza(int id_Cerveza, String cer_Nombre, float cer_Graduacion, int id_Marca) {
+        this.id_Cerveza = id_Cerveza;
+        this.cer_Nombre = cer_Nombre;
+        this.cer_Graduacion = cer_Graduacion;
+        this.id_Marca = id_Marca;
+    }
+    public Cerveza() {
+        this.id_Cerveza = 0;
+        this.cer_Nombre = null;
+        this.cer_Graduacion = 0;
+        this.id_Marca = 0;
+    }
+}
---
+++ PrCerveza/src/model/Fabricante.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Fabricante {
+    private int id_Fabricante;
+    private String fab_Nombre;
+    private int id_Sede;
+
+    public Fabricante(int id_Fabricante, String fab_Nombre, int id_Sede) {
+        this.id_Fabricante = id_Fabricante;
+        this.fab_Nombre = fab_Nombre;
+        this.id_Sede = id_Sede;
+    }
+    public Fabricante() {
+        this.id_Fabricante = 0;
+        this.fab_Nombre = null;
+        this.id_Sede = 0;
+    }
+
+    public int getId_Fabricante() {
+        return id_Fabricante;
+    }
+
+    public void setId_Fabricante(int id_Fabricante) {
+        this.id_Fabricante = id_Fabricante;
+    }
+
+    public String getFab_Nombre() {
+        return fab_Nombre;
+    }
+
+    public void setFab_Nombre(String fab_Nombre) {
+        this.fab_Nombre = fab_Nombre;
+    }
+
+    public int getId_Sede() {
+        return id_Sede;
+    }
+
+    public void setId_Sede(int id_Sede) {
+        this.id_Sede = id_Sede;
+    }
+}
