95c7caeafec13259017d91ad6a8cfe8c81824118
jadeyalli <infinities84@gmail.com>
2024-09-01 15:29:13 -0600

    Formulario Grano

---
+++ Cervezas/src/pk_CRUD/Cls_Grano.java
@@ -0,0 +1,48 @@
+package pk_CRUD;
+import pk_Conexion.C_Conexion;
+import pk_Modelo.Grano;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Cls_Grano {
+
+    private PreparedStatement PS;
+    private C_Conexion CN;
+    private ResultSet RS;
+    DefaultTableModel DTM;
+
+    public Cls_Grano(){
+        this.PS = null;
+        this.CN = new C_Conexion();
+    }
+
+    public int insertarDatos (String gra_nombre, String gra_procedencia){
+        int res = 0;
+        String SQL_Insert = "INSERT INTO grano (gra_nombre, gra_procedencia) VALUES (?,?)";
+        try {
+            PS = CN.getConexion().prepareStatement(SQL_Insert);
+            PS.setString(1,gra_nombre);
+            PS.setString(2,gra_procedencia);
+            res = PS.executeUpdate();
+            if (res > 0){
+                JOptionPane.showMessageDialog(null, "Registro Exitoso", "Succeed", 1);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de inserción" + e, "Error", 0);
+        } finally {
+            PS = null;
+            CN.close();
+        }
+        return res;
+    }
+
+
+
+
+
+
+}
---
+++ Cervezas/src/pk_Conexion/C_Conexion.java
@@ -0,0 +1,49 @@
+package pk_Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class C_Conexion {
+    private static Connection cnx;
+    private static final String DRIVER= "com.mysql.jdbc.Driver";
+    private static final String BD = "cervezas";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/" + BD +
+            "?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public Connection getConexion(){
+        cnx=null;
+        try{
+            Class.forName(DRIVER);
+            cnx=(Connection)DriverManager.getConnection(URL, USER, PASS);
+        } catch(ClassNotFoundException e){
+            System.out.println("Error, no se pudo realizar la conversion de la clase"
+                    + e.getMessage());
+        } catch (SQLException e){
+            System.out.println("Error de conexion" + e.getMessage());
+        }
+        return cnx;
+    }
+
+    public void close(){
+        if(cnx!=null){
+            try {
+                cnx.close();
+            } catch (SQLException e) {
+                System.out.println("Error, no se puede cerrar la conexion"
+                        + e.getMessage());
+            }
+        }
+    }
+
+    public static void main (String[] args){
+        C_Conexion con = new C_Conexion();
+        if(con.getConexion()!= null){
+            System.out.println("Conexion exitosa");
+        } else{
+            System.out.println("No se pudo realizar la conexion con la BD");
+        }
+    }
+}
