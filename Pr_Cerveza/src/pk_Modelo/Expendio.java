2690d7d33eda3e29daf841e4ada11a16ccfe5906
ObethY <obeth4567@gmail.com>
2024-09-11 23:18:07 -0600

    Update Expendio.java
    Modificacion de clase expendio

--- Pr_Cerveza/src/pk_Modelo/Expendio.java
+++ Pr_Cerveza/src/pk_Modelo/Expendio.java
@@ -1,16 +1,27 @@
 package pk_Modelo;
 
+
 public class Expendio {
     private int id_Expendio;
     private String exp_Nombre;
     private String exp_RFC;
-    private String exp_Ubicacion;
+    private String exp_Calle;
+    private int exp_Postal;
+    private String exp_Municipio;
+    private String exp_Estado;
+    private int exp_NumExterior;
 
-    public Expendio(int id_Expendio, String exp_Nombre, String exp_RFC, String exp_Ubicacion) {
+
+    public Expendio(int id_Expendio, String exp_Nombre, String exp_RFC, String exp_Calle, 
+                    int exp_Postal, String exp_Municipio, String exp_Estado, int exp_NumExterior) {
         this.id_Expendio = id_Expendio;
         this.exp_Nombre = exp_Nombre;
         this.exp_RFC = exp_RFC;
-        this.exp_Ubicacion = exp_Ubicacion;
+        this.exp_Calle = exp_Calle;
+        this.exp_Postal = exp_Postal;
+        this.exp_Municipio = exp_Municipio;
+        this.exp_Estado = exp_Estado;
+        this.exp_NumExterior = exp_NumExterior;
     }
 
     public int getId_Expendio() {
@@ -37,11 +48,41 @@ public class Expendio {
         this.exp_RFC = exp_RFC;
     }
 
-    public String getExp_Ubicacion() {
-        return exp_Ubicacion;
+    public String getExp_Calle() {
+        return exp_Calle;
     }
 
-    public void setExp_Ubicacion(String exp_Ubicacion) {
-        this.exp_Ubicacion = exp_Ubicacion;
+
+    public int getExp_Postal() {
+        return exp_Postal;
     }
+
+    public void setExp_Postal(int exp_Postal) {
+        this.exp_Postal = exp_Postal;
+    }
+
+    public String getExp_Municipio() {
+        return exp_Municipio;
+    }
+
+    public void setExp_Municipio(String exp_Municipio) {
+        this.exp_Municipio = exp_Municipio;
+    }
+
+    public String getExp_Estado() {
+        return exp_Estado;
+    }
+
+    public void setExp_Estado(String exp_Estado) {
+        this.exp_Estado = exp_Estado;
+    }
+
+    public int getExp_NumExterior() {
+        return exp_NumExterior;
+    }
+
+    public void setExp_NumExterior(int exp_NumExterior) {
+        this.exp_NumExterior = exp_NumExterior;
+    }
+ 
 }
